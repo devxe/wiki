09853970d385036f65f844d607703b250bedd1ae
김용일 <kyi910705@gmail.com>
2023-04-28 11:30:48 +0900

    👷 Setting: dev, production 구분

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,10 @@ import React, { useEffect } from "react";
 
 export default function Home() {
   useEffect(() => {
-    location.href = "/docs/intro";
+    location.href =
+      process.env.NODE_ENV === "development"
+        ? "/docs/intro"
+        : "/wiki/docs/intro";
   });
 
   return <Layout title="" description="" />;
